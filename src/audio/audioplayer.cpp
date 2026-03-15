7176c317df483db2f0299505940960938352e6ae
Martin Lever <levermartin555@gmail.com>
2026-03-15 14:47:55 +0000

    update: audoplayer class

--- src/audio/audioplayer.cpp
+++ src/audio/audioplayer.cpp
@@ -1,1 +1,27 @@
 #include "audio/audioplayer.hpp"
+
+#include <string>
+
+bool AudioPlayer::AudioPlayer(std::string Path)
+: path_(path) {
+
+};
+
+bool AudioPlayer::load() {
+
+}
+
+bool AudioPlayer::play() {
+
+}
+
+bool AudioPlayer::init() {
+
+}
+
+bool AudioPlayer::configureAsla() {
+    
+}
+
+
+ 
